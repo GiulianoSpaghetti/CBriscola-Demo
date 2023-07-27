62347f066b874721542bdd06c06cc64b4e23a8d8
numer <numer@NUMERONESOFT>
2023-07-27 23:27:16 +0200

    Aggiungere i file di progetto.

---
+++ CBriscola-Demo/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace CBriscola_Demo.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ CBriscola-Demo/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace CBriscola_Demo.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
